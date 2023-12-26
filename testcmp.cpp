17bae6eb147f19c66093ec3fdc04ac9bf25b793f
VariantNeSootvetsvuetZadaiu <vanekpopkinkov577@mail.ru>
2023-12-26 22:00:58 +0500

    Исправил testcmp.cpp

--- testcmp.cpp
+++ testcmp.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
-#include     "mycomplex.h"
+#include "mycomplex.h"
+
 using namespace std;
-int main(){//������������ ������ ����������� �����
- Complex A,B(-4), C(23.0, 45.9);
-cout<<A <<","<< B<<","<<C <<endl; A=B+C;
-cout<<"A=B+C,A="<< A<<endl;
 
-Complex M=B-C;
-cout<< "M = B - C, M = " << M << endl;cout<<"M*A="<<M*A<<endl;
-cout<<"M/4.45="<<M/4.45<<endl;
+int main() {
+    // Инициализация комплексных чисел
+    Complex A, B(-4), C(23.0, 45.9);
 
+    // Вывод комплексных чисел
+    cout << A << "," << B << "," << C << endl;
+
+    // Операция сложения
+    A = B + C;
+    cout << "A = B + C, A = " << A << endl;
+
+    // Операция вычитания
+    Complex M = B - C;
+    cout << "M = B - C, M = " << M << endl;
+    cout << "M * A = " << M * A << endl;
+    cout << "M / 4.45 = " << M / 4.45 << endl;
+
+    // Ввод комплексного числа D
     Complex D;
-  	cout<<"Enter complex number D = "; cin >>D;
- A+=C+D;
-cout << "D = " << D << "\nA = " << A << endl;return 0;}
+    cout << "Enter complex number D = ";
+    cin >> D;
+
+    // Дополнительные операции
+    A += C + D;
+    cout << "D = " << D << "\nA = " << A << endl;
+
+    return 0;
+}
